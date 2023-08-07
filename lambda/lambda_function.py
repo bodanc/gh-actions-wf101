1ccd6c09cebd5408aa36b1f16a23faa5500f9a28
Bogdan Dancu <bogdan@domain.local>
2023-08-07 15:39:32 +0200

    fixed

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n---------------------------------------------'
+    print('Starting functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
         return "World"
